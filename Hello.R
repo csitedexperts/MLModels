e522fdd8c0582a3d7e083e168d7b43d357b72146
CSITEd Experts <csitedexperts@gmail.com>
2018-10-27 21:07:10 -0400

    codes copied from a previous project

---
+++ Hello.R
@@ -0,0 +1,27 @@
+helloStr ="Hello000000 'world" 
+print (helloStr)
+helloStr2 = "Hello000000 'worlddddddddddd" 
+print (helloStr2)
+
+
+print(class(4))
+print(class(4.5))
+print(class(TRUE))
+print(class(F))
+print(class((2>1) && T))
+print(1 + 2i)
+
+print(class(charToRaw("12+12")))
+is.integer(helloStr2)
+as.integer(2)
+as.character("c")
+
+sprintf("4 + 5 = %d", 4 + 5)
+sprintf("4 - 5 = %d", 4 - 5)
+sprintf("4 * 5 = %d", 4 * 5)
+sprintf("4 / 5 = %f", 4 / 5)
+sprintf("4 %% 5 = %d", 4 %% 5)
+sprintf("4 ^ 2 = %d", 4 ^2)
+numbers = c(3, 36, 17, 88)
+numbers[-1]
+length(numbers)
